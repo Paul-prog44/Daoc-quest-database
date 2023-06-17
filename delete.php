a9348fa9ffcde0ee8e7bb8997a498a2686c99eee
Paul-prog44 <saintouenfr@hotmail.com>
2023-06-17 16:15:58 +0200

    Redirection from delete page

--- delete.php
+++ delete.php
@@ -4,4 +4,9 @@ require_once("header.php");
 require("QuestManager.php");
 
 $QuestManager = new QuestManager;
+
+//Delete quest by id
 $QuestManager->delete($_GET["id"]);
+
+//Fonction de redirection vers index.php
+header("Location: index.php");
